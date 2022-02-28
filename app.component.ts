eb9c2c3fccefa70d8d97754b0e6c45619cd8d066
Charmila Tammineedi <100565308+CharmilaTammineedi@users.noreply.github.com>
2022-02-28 16:44:35 +0530

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Practicing-directives';
+  showDetails = false;
+  clicks: number[] = [];
+onDetailsToggle() {
+    this.showDetails = !this.showDetails;
+    this.clicks.push(this.clicks.length + 1)
+  }
+}
